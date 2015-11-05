7f739630617c7e911307fcc91698ac4091b88933
Vincent Bellittera <vjbellittera@gmail.com>
2015-11-04 20:16:29 -0500

    commented out our plan for interactivty in script.js

---
+++ js/script.js
@@ -0,0 +1,43 @@
+//Problem: User interaction doesn't provide desired results.
+//Solution: Add interactivty so the user can manage daily tasks.
+
+//Add a task
+	//when the button is pressed
+	//create a new list item with the text from #new-task:
+		//input (chexkbox)
+		//label
+		//input (text)
+		//button.edit
+		//button.delete
+		//each elements, needs to be modified and appended
+
+
+//Edit a task 
+	//when the edit button is pressed
+	//if the class of the parent is .editMode
+		//switch from .editMode
+		//label text becomes the inputs value
+	//else
+		//switch to .editMode
+		//input value becomes the labels text
+
+	//Toggle .editMode on the parent
+
+
+//Delete a task
+	//when the delete button is pressed 
+		//remove the parent list item from the ul
+
+
+//Mark a task as complete
+	//when the checkbox is checked
+		//append the task list item to the #completed-tasks
+
+
+//Mark a task as incomplete
+	//when the checkbox is unchecked
+		//append the task list item to the #incomplete-tasks
+
+
+
+
